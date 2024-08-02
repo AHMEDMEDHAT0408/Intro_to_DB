1b5f8d5ace59b06e9f7627cbf864069adcc956ec
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-08-02 15:39:15 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,12 +2,10 @@ import mysql.connector
 from mysql.connector import Error
 
 def create_database():
-    cursor = None
-    connection = None
     try:
         connection = mysql.connector.connect(
             host='localhost',
-            user='your_username',
+            user='root',
             password='your_password'
         )
 
@@ -17,14 +15,12 @@ def create_database():
             print("Database 'alx_book_store' created successfully!")
 
     except Error as e:
-        print(f"Error: {e}")  # Handles exceptions of type mysql.connector.Error
+        print(f"Error: {e}")
     
     finally:
-        if cursor is not None:
+        if connection.is_connected():
             cursor.close()
-        if connection is not None and connection.is_connected():
             connection.close()
-            print("MySQL connection is closed")
 
 if __name__ == "__main__":
     create_database()
